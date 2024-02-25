a238a5ca547b522dbba17ea80c57db9eb44d3843
SaurinSavla <saurin.savla@gmail.com>
2024-02-25 10:30:50 +0530

    integration and added styling

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def predict_output(data):
     prediction = model.predict(data)
     return prediction
 
+def get_selected_columns(data):
+    # Replace this with the columns you want to display on the output page
+    return data[['sex', 'age', 'education', 'IQ']]
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -43,11 +47,13 @@ def upload_csv():
         # Now 'df' is your Pandas DataFrame, you can use it as needed
         print(df.head())  # Print the first few rows for demonstration
 
+        patient_info = get_selected_columns(df)
+        
         # Predict output using your model
         output = predict_output(df)
 
         # Pass the output to the HTML template
-        return redirect(url_for('output_page', output=output))
+        return redirect(url_for('output_page', output=output, patient_info=patient_info.to_dict('records')))
 
     return render_template('upload_csv.html')
 
@@ -65,9 +71,25 @@ def predict():
 
     return render_template('predict.html', prediction=result)
 
-@app.route('/doctors/output/<output>')
-def output_page(output):
-    return render_template('output.html', output=output)
+# @app.route('/doctors/output/<output>')
+@app.route('/doctors/output/<output>/<patient_info>')
+def output_page(output, patient_info):
+    # output_val = output[0]
+    output_list = eval(output)
+    # selected_columns_list = eval(selected_columns)
+    patient_val_list = eval(patient_info)
+    # Extract the single value from the list (assuming it's a single-item array)
+    output_value = output_list[0] if output_list else None
+    # print(output_value)
+    # Determine content based on the output value
+    if output_value == 1:
+        content = "The prediction is 1. The patient is more likely to commit suicide"
+    elif output_value == 0:
+        content = "The prediction is 0. The patient is less likely to commit suicide"
+    else:
+        content = "Invalid output value."
+
+    return render_template('output.html', content=content, output=output, patient_info = patient_val_list)
 
 
 if __name__ == '__main__':
