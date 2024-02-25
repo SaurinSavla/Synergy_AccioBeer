0320b080c51f935d46b1e11bd0be900ee0c7d3e7
SaurinSavla <saurin.savla@gmail.com>
2024-02-25 06:12:41 +0530

    initial integration

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template, request
+import numpy as np
+import pandas as pd
+from flask import Flask, render_template, request, redirect, url_for
+import pickle
+from io import StringIO
 
 app = Flask(__name__)
+
+model = pickle.load(open('models/model.pkl', 'rb'))
+
+def predict_output(data):
+    # df = pd.read_csv(StringIO(data))
+    prediction = model.predict(data)
+    return prediction
 
 @app.route('/')
 def home():
@@ -19,20 +30,45 @@ def upload_csv():
     if request.method == 'POST':
         if 'csv_file' not in request.files:
             return "No file part"
-        
+
         file = request.files['csv_file']
 
         if file.filename == '':
             return "No selected file"
 
-        # Here, you can process the uploaded CSV file as needed.
-        # For simplicity, let's just print the content.
-        content = file.read().decode('utf-8')
-        print(f"Content of the uploaded CSV file:\n{content}")
+        # Read CSV content as a Pandas DataFrame
+        csv_data = file.read().decode('utf-8')
+        df = pd.read_csv(StringIO(csv_data))
 
-        return "File uploaded successfully!"
+        # Now 'df' is your Pandas DataFrame, you can use it as needed
+        print(df.head())  # Print the first few rows for demonstration
+
+        # Predict output using your model
+        output = predict_output(df)
+
+        # Pass the output to the HTML template
+        return redirect(url_for('output_page', output=output))
 
     return render_template('upload_csv.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    # int_features = [int(x) for x in request.form.values()]
+    # features = [np.array(int_features)]
+    x=pd.read_csv('C:\Data\DJ\AccioBeer\data\sample2.csv')
+    # output_prediction = model.predict(x)  
+    prediction = model.predict(x) 
+    result = prediction[0]
+    print(result)
+    # print(1)
+
+    return render_template('predict.html', prediction=result)
+
+@app.route('/doctors/output/<output>')
+def output_page(output):
+    return render_template('output.html', output=output)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
